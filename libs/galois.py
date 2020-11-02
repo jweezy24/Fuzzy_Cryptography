1e1042f0290d92e3c461b2e18c98084a600a59d5
Jack <jwest1@luc.edu>
2020-11-02 09:58:17 -0600

    setup_tables now functions correctly

--- libs/galois.py
+++ libs/galois.py
@@ -16,14 +16,19 @@ class GaloisField():
         b = 1
         log = 0
         for i in range(0, 2**self.size):
+            log = i
             self.gflog[b] = log
             self.gfilog[log] = b
             b = b << 1
-            if(b & self.max_num):
+            if(b & self.max_num == 256):
                 b = (b ^ self.prime_poly)
+               
+    
+    def __str__(self):
+        return f"Gflog = {self.gflog}\t Gfilog = {self.gfilog}"
             
 if __name__ == "__main__":
     prime_poly = 0b100011101
     generator = 0b10000011
     test = GaloisField(8, prime_poly, generator)
-
+    print(test)
